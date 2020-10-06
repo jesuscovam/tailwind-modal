f8818a3dd7afcfc2e589332897f83a4fd8386154
jesuscovam <jesuscovam@icloud.com>
2020-10-05 21:46:03 -0500

    exportando algo en index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ModalTutorial from '../src/comps/Modals/Slissto/LoginModal'
+export default function Index() {
+  return(
+    <div className='p-8'>
+    <ModalTutorial/>
+  </div>
+  )
+}
